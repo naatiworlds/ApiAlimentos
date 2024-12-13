350054df67948ae51d7e9d61eb2f2fe4290e020e
naatii <jcorbre855@g.educaand.es>
2024-12-13 09:55:01 +0100

    Initial Commit

---
+++ src/test/java/daw2a/alimentos/AlimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package daw2a.alimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
